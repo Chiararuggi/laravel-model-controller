68cee947461b641bd20d34fc40f3043163f17f8a
chiara.ruggi <chiara.ruggi1998@gmail.com>
2024-01-22 17:34:03 +0100

    done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Movie:</h1>
-@foreach ($movies as $movie)
-        <li>
-            <span>{{$movie->title}}</span>
-            <span>{{$movie->original_title}}</span>
-        </li>
-    @endforeach
+<div class="container">
+    <h1>Movie:</h1>
+    <div class="row">
+        @foreach ($movies as $movie)
+        <div class="col-3">
+            <div class="card my-3">
+                <div class="card-body">
+                    <h5 class="card-title">{{$movie->title}}</h5>
+                    <p class="card-text">{{$movie->original_title}}</p>
+                    <p class="card-text">Nationality: {{$movie->nationality}}</p>
+                    <p class="card-text">Date: {{$movie->date}}</p>
+                    <p class="card-text">Vote: {{$movie->vote}}</p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
